4e6f21973c26c83a7f1c1277354229cb703bf0e0
Daniel Grijuela <danielgrijuela@gmail.com>
2015-09-15 18:33:18 +0200

    Add lodash, users, and full functionality

--- bin/bot.js
+++ bin/bot.js
@@ -1,4 +1,5 @@
 var Bot = require('slackbots');
+var _ = require('lodash');
 
 var settings = {
     token: process.env.TOKEN,
@@ -6,8 +7,29 @@ var settings = {
 };
 var bot = new Bot(settings);
 
+var usersList = ["aaron", "alytxu", "arturo", "carlos", "chema", "cynthia", "dgrijuela", "diana", "fuzzyalej", "jose", "juan", "julian", "linkrbot", "luis", "luisherrero", "miguel", "migueldelmazo", "roman", "russianroulette", "sara", "traductor", "werelax"];
+
+var isChatMessage = function (message) {
+  return message.type === 'message' && Boolean(message.text);
+};
+
+var isChannelConversation = function (message) {
+  return typeof message.channel === 'string' &&
+    message.channel[0] === 'C';
+};
+
+var isMentioningRussianRoulette = function (message) {
+  return message.text.toLowerCase().indexOf('roulette') > -1 ||
+    message.text.toLowerCase().indexOf(bot.name) > -1;
+};
+
+var replyWithRussianRoulette = function (message) {
+  var chosenOne = _.sample(usersList);
+  bot.postMessageToChannel(message.channel, 'The choooooosen one has beeeeeeeen.... -> @' + chosenOne, {as_user: true});
+};
+
 bot.on('message', function(message) {
-  if(message.text.toLowerCase().indexOf('roulette') > -1) {
-    bot.postMessageToChannel(message.channel.name, 'testing', {as_user: true});
+  if (isChatMessage(message) && isChannelConversation(message) && isMentioningRussianRoulette(message)) {
+    replyWithRussianRoulette(message);
   }
 });
